2493f3c63257d5edd10c5f8202f5ef46f7064bf3
samvise85 <luca.samvise@gmail.com>
2015-03-19 16:09:49 +0100

    Corrected BookManagerImpl

--- bookshelf/bookshelf-inmemory-db/src/main/java/it/samvise85/bookshelf/manager/BookManagerImpl.java
+++ bookshelf/bookshelf-inmemory-db/src/main/java/it/samvise85/bookshelf/manager/BookManagerImpl.java
@@ -1,49 +1,24 @@
 package it.samvise85.bookshelf.manager;
 
 import it.samvise85.bookshelf.model.book.Book;
-import it.samvise85.bookshelf.persist.PersistOptions;
 import it.samvise85.bookshelf.persist.clauses.NoProjectionClause;
-import it.samvise85.bookshelf.persist.clauses.ProjectionClause;
 import it.samvise85.bookshelf.persist.inmemory.BookRepository;
+import it.samvise85.bookshelf.persist.inmemory.InMemoryPersistenceUnit;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class BookManagerImpl implements BookManager {
+public class BookManagerImpl extends InMemoryPersistenceUnit<Book>  implements BookManager {
 	@Autowired
 	protected BookRepository repository;
 	
-	@Override
-	public Book get(String id) {
-		return repository.findOne(id);
-	}
-
-	@Override
-	public Book get(String id, ProjectionClause projection) {
-		return repository.findOne(id);
-	}
-
-	@Override
-	public List<Book> getList(PersistOptions options) {
-//		PageRequest pagination = new PageRequest(options.getPagination().getPage(), options.getPagination().getPageSize());
-//		Page<Book> page = repository.findAll(pagination);
-		Iterable<Book> page = repository.findAll();
-		ArrayList<Book> list = new ArrayList<Book>();
-		for(Book b : page)
-			list.add(b);
-		return list;
-	}
-
-	@Override
-	public Book delete(String id) {
-		// TODO Auto-generated method stub
-		return null;
+	public BookManagerImpl() {
+		super(Book.class);
 	}
 
 	@Override
@@ -66,13 +41,18 @@ public class BookManagerImpl implements BookManager {
 			bookToUpdate.setYear(updates.getYear());
 
 		bookToUpdate.setLastModification(new Date());	
-		return repository.save(bookToUpdate);
+		return super.update(bookToUpdate);
 	}
 
 	@Override
 	public Book create(Book objectToSave) {
 		if(objectToSave.getId() == null)
 			objectToSave.setId(objectToSave.getTitle().replaceAll("\\W+", "_"));
-		return repository.save(objectToSave);
+		return super.create(objectToSave);
+	}
+
+	@Override
+	public CrudRepository<Book, String> getRepository() {
+		return repository;
 	}
 }
